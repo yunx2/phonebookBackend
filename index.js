d6e4a309f8f70bf4c4c8abb85b6de69bb6e472d2
Yunyun <celeph4is@gmail.com>
2020-04-27 17:48:55 -0700

    add handlers for crud operations; still need to test and add error handlihng

--- index.js
+++ index.js
@@ -14,37 +14,6 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 
-
-// let persons = [
-  //   { 
-  //     "name": "Arto Hellas", 
-  //     "number": "040-123456",
-  //     "id": 1
-  //   },
-  //   { 
-  //     "name": "Ada Lovelace", 
-  //     "number": "39-44-5323523",
-  //     "id": 2
-  //   },
-  //   { 
-  //     "name": "Dan Abramov", 
-  //     "number": "12-43-234345",
-  //     "id": 3
-  //   },
-  //   { 
-  //     "name": "Mary Poppendieck", 
-  //     "number": "39-23-6423122",
-  //     "id": 4
-  //   }
-  // ];
-
-  // app.get('/info', (req, res) => {
-  //   const entries = persons.length;
-  //   const time = new Date();
-  //   const content = `<div>phonebook has info for ${entries}</div><br />${time}`;
-  //   res.send(content);
-  // });
-
 app.post('/api/persons', (req, res) => {
   const body = req.body;
   if (body.name === undefined || body.number === undefined) {
@@ -83,6 +52,17 @@ app.get('/api/persons/:id', (req, res, next) => {
     })
     .catch(err => { // id doesn't match mongo identifier format
       next(err);
+    });
+});
+
+app.put('api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const update = {name: body.name, number: body.number};
+  Person.findByIdAndUpdate(id, update, {new: true})
+    .then(updated => {
+      const formatted = updated.toJSON();
+      res.send(formatted);
     });
 });
 
